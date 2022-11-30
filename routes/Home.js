04c6443a7c9c27c156a9f8efc1e4c0b5ae3a755b
jemma-mg <jemmamariyageorge@gmail.com>
2022-12-01 01:16:01 +0530

    routes

---
+++ routes/Home.js
@@ -0,0 +1,16 @@
+import destination from "../components/destination/destination";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Trip from "../components/Trip";
+
+function Home() {
+  return (
+    <>
+      <h1>Call The Components</h1>
+      <destination/>
+    </>
+  );
+}
+
+export default Home;
