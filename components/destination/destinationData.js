f8bbe25336a97219280e5963ef81ed90d2b02def
jemma-mg <jemmamariyageorge@gmail.com>
2022-12-01 01:17:39 +0530

    popular destinations component added

---
+++ components/destination/destinationData.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import "./destination/destination.css"
+
+class destinationData extends Component{
+    render(){
+        return(
+            <div className={ this.props.className }>
+            <div className='desc__text'>
+                <h2>{ this.props.heading }</h2>
+                <p>{ this.props.text }</p>
+            </div>
+            <div className='desc__image'>
+                <img alt='img1' src={ this.props.img1 }></img>
+                <img alt='img1' src={ this.props.img2 }></img>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default destinationData
