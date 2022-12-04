60932804d1ece3d029afd1245dae944fbda755dd
Devikaaji <devikaajithkadavithara@gmail.com>
2022-12-04 23:17:37 +0530

    service,contact,hero,home,about

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Your Code Here</h1>
+      <Navbar/>
     </div>
   );
 }
-
-export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,16 @@ import Trip from "../components/Trip";
 function Home() {
   return (
     <>
-      <h1>Call The Components</h1>
+    <Navbar/>
+      <Hero
+        CName="hero"
+        heroImg="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dHJhdmVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        title="Creating Your New Story" 
+        text "Choose Your Favourite Destination"
+        buttonText="Travel Plan"
+        url="/"
+        btnClass="show"
+      />
     </>
   );
 }
