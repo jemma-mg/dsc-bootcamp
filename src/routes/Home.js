62a3fb3eb893db7b359a830e0dc6ae92c62dab03
jemma-mg <jemmamariyageorge@gmail.com>
2022-12-05 00:06:19 +0530

    file change

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import destination from "../components/destination/destination";
-import Footer from "../components/Footer";
+import footer from "../components/footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import trip from "../components/trip/trip";
@@ -12,7 +12,7 @@ function Home() {
         CName="hero"
         heroImg="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dHJhdmVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
         title="Creating Your New Story" 
-        text "Choose Your Favourite Destination"
+        text="Choose Your Favourite Destination"
         buttonText="Travel Plan"
         url="/"
         btnClass="show"
