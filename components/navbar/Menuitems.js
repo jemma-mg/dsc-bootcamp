3eff3c497c6be85a5d5b150bdc424a0f522d5469
poojarajev <poojarajeev8848@gmail.com>
2022-12-05 15:26:12 +0530

    menu, navbar

---
+++ components/navbar/Menuitems.js
@@ -0,0 +1,30 @@
+export const MenuItems=[
+    {
+        title: "Home",
+        url:"/",
+        cHame: "nav-links",
+        icon:"fa-solid fa-house-user"
+    
+    },
+
+{
+    title: "About",
+    url: "/about",
+    cName: "nav-links",
+    icon: "fa-solid fa-circle-used",
+    title: "Home",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user",
+},
+{
+    title: "Contact",
+    url: "/contact",
+    cName: "nav-links",
+    icon: "fa-solid fa-address-book",
+    title:"Contact",
+    url: "/",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user",
+}
+
+]   
