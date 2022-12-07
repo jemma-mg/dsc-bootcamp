f586b731d5b390f8b67c4b8fcc67fad8c64f2653
jemma-mg <jemmamariyageorge@gmail.com>
2022-12-07 22:39:47 +0530

    navbar added

---
+++ src/components/navbar/Menuitems.js
@@ -0,0 +1,27 @@
+export const MenuItems=[
+    {
+        title: "Home",
+        url: "/",
+        class__name: "nav__links",
+        icon: "fa-solid fa-house-user"
+    
+    },
+    {
+        title: "About",
+        url: "/about",
+        class__name: "nav__links",
+        icon: "fa-solid fa-circle-info",
+    },
+    {
+        title: "Service",
+        url: "/service",
+        class__name: "nav__links",
+        icon: "fa-solid fa-briefcase",
+    },
+    {
+        title: "Contact",
+        url: "/contact",
+        class__name: "nav__links",
+        icon: "fa-solid fa-address-book",
+    }
+]   
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,34 @@
+import "./navbar.css";
+import { Component } from "react";
+import { MenuItems } from "./Menuitems";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  state = { clicked:false };
+  // Click Function
+  handleClick = ()=>{
+    this.setState({clicked: !this.state.clicked})
+  }
+  // NavBar
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar__logo">Tripeasy</h1>
+        <div className="menu__icons" onClick={this.handleClick}>
+          <i className={ this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className={this.state.clicked ? ".nav__menu active" : ".nav__menu"}>
+          { MenuItems.map((item,index)=>{
+            return(
+              <li key={index}>
+                <Link className={item.class__name}><i class={item.icon}></i>{item.title}</Link>
+              </li>
+            )
+          }) }
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
