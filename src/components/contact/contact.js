ff57f91aa81393fd8881ee8519ae4d1a62d06448
jemma-mg <jemmamariyageorge@gmail.com>
2022-12-12 17:12:45 +0530

    contact form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,7 +3,7 @@ import "./contact.css"
 function ContactForm(){
     return(
         <div className="form__container">
-            <h1>Send a message to ss</h1>
+            <h1>Send a message to us</h1>
             <form>
                 <input placeholder="Name"/>
                 <input placeholder="Email"/>
