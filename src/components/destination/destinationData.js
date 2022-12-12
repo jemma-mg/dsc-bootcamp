9be415843f3871f13a2ea0aa43690583ef4514af
jemma-mg <jemmamariyageorge@gmail.com>
2022-12-12 17:11:00 +0530

    destination section

--- src/components/destination/destinationData.js
+++ src/components/destination/destinationData.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import "./destination.css"
 
-class destinationData extends Component{
+class DestinationData extends Component{
     render(){
         return(
             <div className={ this.props.className }>
@@ -11,11 +11,11 @@ class destinationData extends Component{
             </div>
             <div className='desc__image'>
                 <img alt='img1' src={ this.props.img1 }></img>
-                <img alt='img1' src={ this.props.img2 }></img>
+                <img alt='img2' src={ this.props.img2 }></img>
             </div>
             </div>
         )
     }
 }
 
-export default destinationData
+export default DestinationData
